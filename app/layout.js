c324f89585804f14251804a4eb08d544b6971fd2
Anurag Kamra <anuragkamra7@gmail.com>
2024-03-28 16:22:30 +0530

    setup nav bar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
-import Nav from '@/(components)/nav';
+import Nav from './(components)/Nav';
 import './globals.css';
 import { Inter } from 'next/font/google';
+
+//icons:
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,8 +19,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Nav />
-        {children}
+        <div className='flex flex-col h-screen max-h-screen'>
+          <Nav />
+
+          <div className='flex-grow overflow-y-auto bg-page text-default-text'>
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
