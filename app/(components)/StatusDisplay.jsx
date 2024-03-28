58d86773a32a6bba90dde61fd7d0eb448bdbc243
Anurag Kamra <anuragkamra7@gmail.com>
2024-03-28 16:52:59 +0530

    completed basic structure

---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StatusDisplay = () => {
+  return (
+    <span className='inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200 hover:bg-green-400 cursor-pointer'>
+      Done
+    </span>
+  );
+};
+
+export default StatusDisplay;
